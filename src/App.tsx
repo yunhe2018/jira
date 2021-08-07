3eaec980bd47de98773c669b363cd6c9618981ff
wangzhaoan <zhaoan2018@163.com>
2021-08-07 15:41:41 +0800

    123

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,15 @@ function App() {
 
       <header className="App-header"
       
+
       >
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx
-            </code> 
+        <p> Edit <code>src/App.tsx </code> 
           and save to reload.
         </p>
+
         <a 
-        className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+        className="App-link" href="https://reactjs.org" target="_blank"
           rel="noopener noreferrer"
           >
           Learn React
