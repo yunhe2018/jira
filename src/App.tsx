f210b1489e85412023bd9d096a9cd964f751c617
wangzhaoan <zhaoan2018@163.com>
2021-08-08 18:53:52 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(123);
+
+  const a = 6;
+  const b = 8;
+  const c = 9;
+  console.log(12);
+  const aaa = (params: number) => {
     console.log(123);
-
-    const a = 6;
-    const b = 8;
-    const c = 9;
-
-    console.log(12);
-    const aaa = (params: number) => {
-        console.log(123);
-    };
-    return (
-        <div className="App">
-            <header className="App-header">
-                13345 123
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    {' '}
-                    Edit <code>src/App.tsx</code>
-                    and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        13345 123
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {' '}
+          Edit <code>src/App.tsx</code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
