2fd9a43eed96d11d0636e3bedb3d30575d637c29
wangzhaoan <zhaoan2018@163.com>
2021-08-08 17:46:13 +0800

    12

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const a = 6
+  const aaa = () => {
+    console.log(123);
+
+    
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
 
+
       <header className="App-header"
     
   
