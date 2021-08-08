191f4e8de2880901f61db71b1dad356e7a2f6b04
wangzhaoan <zhaoan2018@163.com>
2021-08-08 18:29:25 +0800

    3

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(123)
-  
-  const a = 6
-  const b= 8;
+  console.log(123);
 
-  const  aaa = (params:number) => {
-    console.log(123)
-  }
+  const a = 6;
+  const b = 8;
+
+  const aaa = (params: number) => {
+    console.log(123);
+  };
   return (
     <div className="App">
-
-
-      <header className="App-header"
-    
-  
-      >13345
-
-      123
-
-        <img src={logo}
-         className="App-logo"
-         alt="logo" />
-        <p> Edit <code>src/App.tsx </code> 
+      <header className="App-header">
+        13345 123
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {' '}
+          Edit <code>src/App.tsx </code>
           and save to reload.
         </p>
-
-        <a 
-        className="App-link" href="https://reactjs.org" target="_blank"
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
           rel="noopener noreferrer"
-          >
+        >
           Learn React
-
         </a>
       </header>
     </div>
